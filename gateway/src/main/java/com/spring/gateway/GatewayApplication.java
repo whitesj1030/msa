d6f8a6d0f8be826600b0e3dc788cf1ae53682e53
whitesj1030 <ycpopo@naver.com>
2020-06-11 17:46:21 +0900

    gateway first

---
+++ gateway/src/main/java/com/spring/gateway/GatewayApplication.java
@@ -0,0 +1,40 @@
+package com.spring.gateway;
+
+import com.spring.gateway.filters.GatewayErrorFilter;
+import com.spring.gateway.filters.GatewayPostFilter;
+import com.spring.gateway.filters.GatewayPreFilter;
+import com.spring.gateway.filters.GatewayRouteFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public GatewayPreFilter preFilter() {
+        return new GatewayPreFilter();
+    }
+
+    @Bean
+    public GatewayPostFilter postFilter() {
+        return new GatewayPostFilter();
+    }
+
+    @Bean
+    public GatewayRouteFilter routeFilter() {
+        return new GatewayRouteFilter();
+    }
+
+    @Bean
+    public GatewayErrorFilter errorFilter() {
+        return new GatewayErrorFilter();
+    }
+}
